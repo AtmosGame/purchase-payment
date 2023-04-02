ab7b576a9db96bf4cca2eb3cace275151e9b7b7f
Dhafin Raditya Juliawan <dhafin.raditya@ui.ac.id>
2023-04-02 21:15:23 +0700

    test: add dummy test

---
+++ src/test/java/id/ac/ui/cs/advprog/purchasepayment/PurchasePaymentApplicationTests.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.purchasepayment;
+
+import id.ac.ui.cs.advprog.purchasepayment.controller.DummyController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+@SpringBootTest
+class PurchasePaymentApplicationTests {
+
+    @Autowired
+    private DummyController myController;
+
+    @Test
+    void contextLoads() {
+        assertThat(myController).isNotNull();
+    }
+}
