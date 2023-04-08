d50624108d73497392cc7f1b5be4bd12a2eae763
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-04-08 19:17:03 +0700

    Change controller name

--- src/main/java/id/ac/ui/cs/advprog/purchasepayment/controller/PurchaseAndPaymentController.java
+++ src/main/java/id/ac/ui/cs/advprog/purchasepayment/controller/PurchaseAndPaymentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class PaymentAndPurchaseController {
+public class PurchaseAndPaymentController {
 
     @GetMapping("/test")
     public ResponseEntity<String> sayTest() {
