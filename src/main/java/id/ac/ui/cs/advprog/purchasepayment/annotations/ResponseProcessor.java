1a2ad36106fc100bfc8d53146b5b67d04e30f98c
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-04-08 16:23:43 +0700

    Add annotations

---
+++ src/main/java/id/ac/ui/cs/advprog/purchasepayment/annotations/ResponseProcessor.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.purchasepayment.annotations;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface ResponseProcessor {
+}
