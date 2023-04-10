bf81e26e00e64b3bbd1701209864a86dbee8f966
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-04-10 23:34:59 +0700

    Remove unused import

--- src/test/java/id/ac/ui/cs/advprog/purchasepayment/PurchasePaymentApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/purchasepayment/PurchasePaymentApplicationTests.java
@@ -1,14 +1,11 @@
 package id.ac.ui.cs.advprog.purchasepayment;
 
 import id.ac.ui.cs.advprog.purchasepayment.controller.DummyController;
-import id.ac.ui.cs.advprog.purchasepayment.web.logic.UpdateCartLogic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 
 @SpringBootTest
