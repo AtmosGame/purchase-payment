63a0368a99f113f28b8a841e7f794b4f83ba4777
Dimitri Prima Nugraha <dimitri.prima@ui.ac.id>
2023-04-29 23:01:14 +0700

    Refactoring checkout and adding invalidate feature

--- src/main/java/id/ac/ui/cs/advprog/purchasepayment/ports/CheckoutRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/purchasepayment/ports/CheckoutRepository.java
@@ -1,11 +1,11 @@
 package id.ac.ui.cs.advprog.purchasepayment.ports;
 
-import id.ac.ui.cs.advprog.purchasepayment.models.Cart;
 import id.ac.ui.cs.advprog.purchasepayment.models.Checkout;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CheckoutRepository extends CrudRepository<Checkout, Integer>{
+    Optional<Checkout> findByUsername(String username);
+
 }
