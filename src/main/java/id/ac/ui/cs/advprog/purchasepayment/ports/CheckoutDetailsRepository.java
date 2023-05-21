34750f0218eee08f9db4fea9ee38244cecae30eb
Dimitri Prima Nugraha <dimitri.prima@ui.ac.id>
2023-05-21 09:44:41 +0700

    Fixing checkout implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/purchasepayment/ports/CheckoutDetailsRepository.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.purchasepayment.ports;
+
+import id.ac.ui.cs.advprog.purchasepayment.models.CartDetails;
+import id.ac.ui.cs.advprog.purchasepayment.models.CheckoutDetails;
+import jakarta.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CheckoutDetailsRepository extends CrudRepository<CheckoutDetails, Integer> {
+
+}
