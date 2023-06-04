029e5b1517a89fb4b55709a859c3cbfe579f727e
Dhafin Raditya Juliawan <dhafin.raditya@ui.ac.id>
2023-06-04 20:31:52 +0700

    add unit test for new validation

--- src/test/java/id/ac/ui/cs/advprog/purchasepayment/usecase/AddSecretTokenImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/purchasepayment/usecase/AddSecretTokenImplTest.java
@@ -1,18 +1,34 @@
 package id.ac.ui.cs.advprog.purchasepayment.usecase;
 
 import id.ac.ui.cs.advprog.purchasepayment.dto.AddSecretTokenRequest;
+import id.ac.ui.cs.advprog.purchasepayment.exceptions.TokenAlreadyExistException;
 import id.ac.ui.cs.advprog.purchasepayment.models.SecretToken;
 import id.ac.ui.cs.advprog.purchasepayment.ports.SecretTokenRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class AddSecretTokenImplTest {
+    @Mock
+    private SecretTokenRepository secretTokenRepository;
+
+    @Mock
+    private AddSecretTokenImpl addSecretToken;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        addSecretToken = new AddSecretTokenImpl(secretTokenRepository);
+    }
 
     @Test
     void testAddSecretTokenByRequest() {
-        SecretTokenRepository secretTokenRepository = mock(SecretTokenRepository.class);
         AddSecretTokenRequest request = AddSecretTokenRequest.builder()
                 .tokenName("testToken")
                 .build();
@@ -30,4 +46,23 @@ class AddSecretTokenImplTest {
         verify(secretTokenRepository, times(1)).save(any(SecretToken.class));
         assertEquals(expectedToken, result);
     }
+
+    @Test
+    void testAddSecretTokenByRequestTokenAlreadyExist() {
+        // Arrange
+        AddSecretTokenRequest request = AddSecretTokenRequest.builder()
+                .tokenName("secret-token")
+                .build();
+
+        when(secretTokenRepository.existsByTokenName(request.getTokenName()))
+                .thenReturn(true);
+
+        // Act & Assert
+        assertThrows(TokenAlreadyExistException.class,
+                () -> addSecretToken.addSecretTokenByRequest(request));
+
+        verify(secretTokenRepository, times(1)).existsByTokenName(request.getTokenName());
+        verify(secretTokenRepository, never()).save(any(SecretToken.class));
+    }
 }
+
